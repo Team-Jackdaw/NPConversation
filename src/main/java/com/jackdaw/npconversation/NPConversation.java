3c2c689b4a81494a39e7ac972375daffafefb7ed
WDRshadow <i@wdr.im>
2024-03-26 21:58:33 +0100

    initial commit

---
+++ src/main/java/com/jackdaw/npconversation/NPConversation.java
@@ -0,0 +1,10 @@
+package com.jackdaw.npconversation;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NPConversation implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
